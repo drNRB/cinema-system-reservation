fe542458e0b084da39ff1a8f913b090a0f690b0c
Bernard <beniotaborski@gmail.com>
2026-05-15 12:27:51 +0200

    test: add concurrency integration test for reservation service using Testcontainers

--- src/test/java/com/portfolio/cinema_system_reservation/CinemaSystemReservationApplicationTests.java
+++ src/test/java/com/portfolio/cinema_system_reservation/CinemaSystemReservationApplicationTests.java
@@ -2,9 +2,28 @@ package com.portfolio.cinema_system_reservation;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 @SpringBootTest
+@Testcontainers
 class CinemaSystemReservationApplicationTests {
+
+    @Container
+    static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:16")
+            .withDatabaseName("cinema_test_db")
+            .withUsername("test_user")
+            .withPassword("test_pass");
+
+    @DynamicPropertySource
+    static void configureProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", postgres::getJdbcUrl);
+        registry.add("spring.datasource.username",postgres::getUsername);
+        registry.add("spring.datasource.password", postgres::getPassword);
+    }
 
 	@Test
 	void contextLoads() {
--- src/test/java/com/portfolio/cinema_system_reservation/service/ReservationServiceConcurrencyTest.java
+++ src/test/java/com/portfolio/cinema_system_reservation/service/ReservationServiceConcurrencyTest.java
@@ -1,4 +1,123 @@
 package com.portfolio.cinema_system_reservation.service;
 
+import com.portfolio.cinema_system_reservation.dto.CreateReservationRequest;
+import com.portfolio.cinema_system_reservation.exceptions.SeatAlreadyReservedException;
+import com.portfolio.cinema_system_reservation.model.Hall;
+import com.portfolio.cinema_system_reservation.model.Movie;
+import com.portfolio.cinema_system_reservation.model.Screening;
+import com.portfolio.cinema_system_reservation.model.Seat;
+import com.portfolio.cinema_system_reservation.repository.*;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+@Testcontainers
 public class ReservationServiceConcurrencyTest {
+
+    @Container
+    static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:16")
+            .withDatabaseName("testdb")
+            .withUsername("testuser")
+            .withPassword("testpass");
+
+    @DynamicPropertySource
+    static void configureProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", postgres::getJdbcUrl);
+        registry.add("spring.datasource.username", postgres::getUsername);
+        registry.add("spring.datasource.password", postgres::getPassword);
+        registry.add("spring.jpa.hibernate.ddl-auto", () -> "update");
+    }
+    @Autowired private ReservationService reservationService;
+    @Autowired private MovieRepository movieRepository;
+    @Autowired private HallRepository hallRepository;
+    @Autowired private ScreeningRepository screeningRepository;
+    @Autowired private SeatRepository seatRepository;
+    @Autowired private ReservationRepository reservationRepository;
+    @Autowired private ReservedSeatRepository reservedSeatRepository;
+
+
+
+    @Test
+    void shouldPreventDoubleBookingUnderConcurrentLoad() throws InterruptedException {
+        Movie movie = movieRepository.save(new Movie("Interstellar", 169));
+        Hall hall = hallRepository.save(new Hall("Sala 1"));
+        Seat seat = seatRepository.save(new Seat(hall, 5, 10));
+
+
+        Screening screening = screeningRepository.save(
+                new Screening(movie, hall, LocalDateTime.now().plusDays(2))
+        );
+
+        Long realScreeningId = screening.getId();
+        Long realSeatId = seat.getId();
+
+        CreateReservationRequest request1 = new CreateReservationRequest(realScreeningId, List.of(realSeatId), "User A");
+        CreateReservationRequest request2 = new CreateReservationRequest(realScreeningId, List.of(realSeatId), "User B");
+
+        int numberOfThreads = 2;
+        ExecutorService executorService = Executors.newFixedThreadPool(numberOfThreads);
+
+        CountDownLatch startLatch = new CountDownLatch(1);
+        CountDownLatch endLatch = new CountDownLatch(numberOfThreads);
+
+        AtomicInteger successfulReservations = new AtomicInteger(0);
+        AtomicInteger failedReservations = new AtomicInteger(0);
+
+        Runnable task1 = createReservationTask(request1, startLatch, endLatch, successfulReservations, failedReservations);
+        Runnable task2 = createReservationTask(request2, startLatch, endLatch, successfulReservations, failedReservations);
+
+        executorService.submit(task1);
+        executorService.submit(task2);
+
+        startLatch.countDown();
+
+        endLatch.await();
+        executorService.shutdown();
+
+
+        assertEquals(1, successfulReservations.get(), "Tylko jedna rezerwacja powinna się udać.");
+        assertEquals(1, failedReservations.get(), "Druga rezerwacja powinna rzucić wyjątek SeatAlreadyReservedException.");
+
+        long totalReservationsInDb = reservationRepository.count();
+        assertEquals(1, totalReservationsInDb, "W bazie danych powinna znajdować się tylko jedna rezerwacja.");
+    }
+
+    private Runnable createReservationTask(CreateReservationRequest request,
+                                           CountDownLatch startLatch,
+                                           CountDownLatch endLatch,
+                                           AtomicInteger successCount,
+                                           AtomicInteger failCount) {
+        return () -> {
+            try {
+                startLatch.await();
+                reservationService.create(request);
+                successCount.incrementAndGet();
+            } catch (SeatAlreadyReservedException e) {
+                failCount.incrementAndGet();
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            } finally {
+                endLatch.countDown();
+            }
+        };
+    }
+
+
+
+
 }
